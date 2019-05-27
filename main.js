ab34cc888add744aa8a46f272b8b3b42939ab0f4
Lou80 <lourdesramirez@yahoo.com>
2019-05-26 21:13:15 -0300

    explorando menu

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
+const url = 'https://api.themoviedb.org/3/movie/';
+const apiKey = '?api_key=9efd0ea0ec598fc52a2db7991ba2e31b';
+const paginaActual = 1;
+/*
 const fetchHome = category => 
-fetch(`https://api.themoviedb.org/3/movie/${category}?api_key=9efd0ea0ec598fc52a2db7991ba2e31b`)
+
+fetch(`${url}${category}${apiKey}`)
     .then(res => res.json())
-    .then(data => {
-        const firstFiveMovies = data.results.slice(0,5);
+    .then(movies => {
+        const firstFiveMovies = movies.results.slice(0,5);
         document.getElementById(`${'movies_list_'}${category}`)
         .innerHTML = firstFiveMovies.map(movie =>`<li class="movies_item" id="${movie.id}><a href="" class="movies_item_link"><div class="movies_item_poster"><img src="https://image.tmdb.org/t/p/original${movie.poster_path}"></div><div class="movies_item_content"><p class="movies_item_title">${movie.original_title}</p></div></a></li>`).join('');
     }
@@ -11,4 +16,19 @@ fetchHome('popular');
 fetchHome('top_rated');
 fetchHome('upcoming');
 fetchHome('now_playing');
+*/
 
+const getAll = category =>
+
+fetch(`${url}${category}${apiKey}&page=${paginaActual}`)
+    .then(res => res.json())
+    .then(movies => {
+        const allMovies = movies.results;
+        document.getElementById(`${'movies_list_'}${category}`)
+        .innerHTML = allMovies.map(movie => `<li class="movies_item" id="${movie.id}><a href="" class="movies_item_link"><div class="movies_item_poster"><img src="https://image.tmdb.org/t/p/original${movie.poster_path}"></div><div class="movies_item_content"><p class="movies_item_title">${movie.original_title}</p></div></a></li>`).join('');
+        document.getElementById('main_image').classList.add('hidden');
+        document.getElementById(`${'button_'}${category}`).classList.remove('hidden');
+        
+    }
+);
+getAll('popular');
