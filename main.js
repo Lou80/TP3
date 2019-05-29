929ffeb3a27eb39083b2af5343adebe75b59e86c
Lou80 <lourdesramirez@yahoo.com>
2019-05-29 19:24:34 -0300

    getting more efficient :)

--- main.js
+++ main.js
@@ -15,7 +15,10 @@ fetch(`${url}${category}${apiKey}`)
     .then(movies => {
         const firstFiveMovies = movies.results.slice(0,5);
         document.getElementById(`${'movies_list_'}${category}`)
-        .innerHTML = firstFiveMovies.map(movie =>`<li class="movies_item" id="${movie.id}"><div class="movies_item_poster"><img src="https://image.tmdb.org/t/p/original${movie.poster_path}"></div><div class="movies_item_content"><p class="movies_item_title">${movie.original_title}</p></div></li>`).join('');
+        .innerHTML = firstFiveMovies.map(movie =>`<li class="movies_item" id="${movie.id}">
+        <div class="movies_item_poster"><img src="https://image.tmdb.org/t/p/original${movie.poster_path}">
+        </div><div class="movies_item_content"><p class="movies_item_title">${movie.original_title}</p></div></li>`)
+        .join('');
     });
 };
 
@@ -28,7 +31,8 @@ fetchHome('now_playing');
 
 getHome();
 
-const getCategory = category => {
+
+const fetchCategory = category => {
 mainScreen.classList.add('hidden');  
 mainImage.classList.add('hidden');
 container.innerHTML = `<div class="wrapper"><header class="movies_header">
@@ -38,45 +42,62 @@ container.innerHTML = `<div class="wrapper"><header class="movies_header">
 <ul class="movies_list" id="${'movies_list_'}${category}"></ul>
 <div class="button"><button id="${category}" class="load_more">LOAD MORE</button></div>
 </div>`;
+const moviesTitle = document.querySelector('h2');
+moviesTitle.innerText = `${category}${' Movies'}`.replace('_', ' ');
+
 fetch(`${url}${category}${apiKey}&page=${paginaActual}`)
     .then(res => res.json())
     .then(movies => {
         const allMovies = movies.results;
-        //console.log(allMovies);
         document.getElementById(`${'movies_list_'}${category}`)
         .innerHTML = allMovies.map(movie => `<li class="movies_item" id="${movie.id}">
         <div class="movies_item_poster"><img src="https://image.tmdb.org/t/p/original${movie.poster_path}">
         </div><div class="movies_item_content"><p class="movies_item_title">${movie.original_title}</p></div></li>`)
         .join('');
-        document.querySelector('.movies_link').innerText = `${movies.total_results-1}${' results'}`
+        document.querySelector('.movies_link').innerText = `${movies.total_results}${' results'}`
                 
     }
 );
 document.querySelector('button').onclick = function () {
     paginaActual += 1;
-    getCategory(`${this.id}`);
+    fetchCategory(`${this.id}`);
     }
 
 };
-//getCategory('top_rated');
+
+getCategory = (category) => {
+    paginaActual = 1;
+    fetchCategory(category);
+};
 
 document.getElementById('logo').onclick = function () {
     getHome();
 };
 document.getElementById('menu_popular').onclick = function () {
     getCategory('popular');
-    document.querySelector('h2').innerText = 'Popular Movies';
 };
 document.getElementById('menu_top_rated').onclick = function () {
     getCategory('top_rated');
-    document.querySelector('h2').innerText = 'Top Rated Movies';
 };
 document.getElementById('menu_upcoming').onclick = function () {
     getCategory('upcoming');
-    document.querySelector('h2').innerText = 'Upcoming Movies';
 };
 document.getElementById('menu_now_playing').onclick = function () {
     getCategory('now_playing');
-    document.querySelector('h2').innerText = 'Now Playing Movies'
 };
 
+document.getElementById('movies_link_popular').onclick = function () {
+    getCategory('popular');
+};
+
+document.getElementById('movies_link_top_rated').onclick = function () {
+    getCategory('top_rated');
+};
+
+document.getElementById('movies_link_upcoming').onclick = function () {
+    getCategory('upcoming');
+};
+
+document.getElementById('movies_link_now_playing').onclick = function () {
+    getCategory('now_playing');
+};
