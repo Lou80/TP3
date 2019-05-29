6ea8e505d0d4afa2cdf0b86c09dc9c4be761cd6f
Lou80 <lourdesramirez@yahoo.com>
2019-05-28 23:30:23 -0300

    load more y total results

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const url = 'https://api.themoviedb.org/3/movie/';
 const apiKey = '?api_key=9efd0ea0ec598fc52a2db7991ba2e31b';
-const paginaActual = 1;
+let paginaActual = 1;
 const mainScreen = document.getElementById('main_screen');
 const mainImage = document.getElementById('main_image');
 const container = document.getElementById('container');
+
 
 const fetchHome = category => {
 mainImage.classList.remove('hidden');
@@ -14,7 +15,7 @@ fetch(`${url}${category}${apiKey}`)
     .then(movies => {
         const firstFiveMovies = movies.results.slice(0,5);
         document.getElementById(`${'movies_list_'}${category}`)
-        .innerHTML = firstFiveMovies.map(movie =>`<li class="movies_item" id="${movie.id}"><a href="" class="movies_item_link"><div class="movies_item_poster"><img src="https://image.tmdb.org/t/p/original${movie.poster_path}"></div><div class="movies_item_content"><p class="movies_item_title">${movie.original_title}</p></div></a></li>`).join('');
+        .innerHTML = firstFiveMovies.map(movie =>`<li class="movies_item" id="${movie.id}"><div class="movies_item_poster"><img src="https://image.tmdb.org/t/p/original${movie.poster_path}"></div><div class="movies_item_content"><p class="movies_item_title">${movie.original_title}</p></div></li>`).join('');
     });
 };
 
@@ -27,30 +28,37 @@ fetchHome('now_playing');
 
 getHome();
 
-
 const getCategory = category => {
 mainScreen.classList.add('hidden');  
 mainImage.classList.add('hidden');
 container.innerHTML = `<div class="wrapper"><header class="movies_header">
     <h2 class="movies_title"></h2>
-    <div class="movies_link">View all<a href=""><div class="movies_link_icon">
-    </div></a></div></header>
+    <div class="movies_link"><div class="movies_link_icon">
+    </div></div></header>
 <ul class="movies_list" id="${'movies_list_'}${category}"></ul>
-<button id="button_${category}" class="load_more"></button>
-</div>`
+<div class="button"><button id="${category}" class="load_more">LOAD MORE</button></div>
+</div>`;
 fetch(`${url}${category}${apiKey}&page=${paginaActual}`)
     .then(res => res.json())
     .then(movies => {
         const allMovies = movies.results;
+        //console.log(allMovies);
         document.getElementById(`${'movies_list_'}${category}`)
         .innerHTML = allMovies.map(movie => `<li class="movies_item" id="${movie.id}">
         <div class="movies_item_poster"><img src="https://image.tmdb.org/t/p/original${movie.poster_path}">
         </div><div class="movies_item_content"><p class="movies_item_title">${movie.original_title}</p></div></li>`)
         .join('');
+        document.querySelector('.movies_link').innerText = `${movies.total_results-1}${' results'}`
                 
     }
 );
+document.querySelector('button').onclick = function () {
+    paginaActual += 1;
+    getCategory(`${this.id}`);
+    }
+
 };
+//getCategory('top_rated');
 
 document.getElementById('logo').onclick = function () {
     getHome();
@@ -62,15 +70,13 @@ document.getElementById('menu_popular').onclick = function () {
 document.getElementById('menu_top_rated').onclick = function () {
     getCategory('top_rated');
     document.querySelector('h2').innerText = 'Top Rated Movies';
-
 };
 document.getElementById('menu_upcoming').onclick = function () {
     getCategory('upcoming');
     document.querySelector('h2').innerText = 'Upcoming Movies';
-
 };
 document.getElementById('menu_now_playing').onclick = function () {
     getCategory('now_playing');
     document.querySelector('h2').innerText = 'Now Playing Movies'
-
 };
+
